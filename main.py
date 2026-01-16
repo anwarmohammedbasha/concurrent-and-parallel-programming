485d8c06847e3d5922e332f4acdd83fce974eb68
Anwar <anwarmohammedbasha@gmail.com>
2026-01-16 17:28:56 +0530

    Created threading class

--- main.py
+++ main.py
@@ -1,34 +1,18 @@
 import time
-import threading
 
 
-def calculate_sum_of_squares(n):
-    sum_of_squares = 0
-    for i in range(n):
-        sum_of_squares += i ** 2
-
-    print(sum_of_squares)
-
-
-def sleep_a_little(seconds):
-    time.sleep(seconds)
-
+from workers.SquaredSumWorkers import SquaredSumWorker
+from workers.SleepyWorkers import SleepyWorker
 
 def main():
 
     start_time = time.time()
 
-    current_threads = []
-
     for i in range(5):
         max_value = (i+1) * 1000000
-        t = threading.Thread(
-            target=calculate_sum_of_squares, args=(max_value, ))
-        t.start()
-        current_threads.append(t)
+        squaredSumWorker = SquaredSumWorker(n=max_value)
 
-    for i in range(len(current_threads)):
-        current_threads[i].join()
+    squaredSumWorker.join()
 
     print("Calculating sum of squares took:",
           round(time.time() - start_time, 1))
@@ -36,12 +20,9 @@ def main():
     start_time = time.time()
 
     for seconds in range(1, 6):
-        t = threading.Thread(target=sleep_a_little, args=(seconds,))
-        t.start()
-        current_threads.append(t)
-        
-    for i in range(len(current_threads)):
-        current_threads[i].join()
+        sleepyWorker = SleepyWorker(seconds=seconds)
+    
+    sleepyWorker.join()
 
     print("Sleep took:", round(time.time() - start_time, 1))
 
